54afe88905af2c0f7014ea83e54df5040001da01
fekete102 <fekete782gmail.com>
2021-09-20 13:13:23 +0200

    első

---
+++ DiakBeker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ifszerkezet
+{
+    class DiakBeker
+    {
+        public string Nev { get; set;}
+
+        public int Eletkor { get; set;}
+
+        public DiakBeker(string nev,int eletKor)
+        {
+            this.Nev = nev;
+            this.Eletkor = eletKor;
+        }
+
+        public void AdatKiir()
+        {
+            Console.WriteLine($"Név: {this.Nev} és életkor: {this.Eletkor}");
+        }
+    }
+}
